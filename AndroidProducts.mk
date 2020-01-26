3d3c1af5794fa25f83be31d5726a73d1b97bf0d7
Jackeagle <jackeagle102@gmail.com>
2020-01-26 18:39:53 +0100

    enchilada: Blissify
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018-2019 The LineageOS Project
+# Copyright (C) 2018-2019 The blissOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_enchilada.mk
+    $(LOCAL_DIR)/bliss_enchilada.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_enchilada-user \
-    lineage_enchilada-userdebug \
-    lineage_enchilada-eng
+    bliss_enchilada-user \
+    bliss_enchilada-userdebug \
+    bliss_enchilada-eng
